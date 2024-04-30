7da12468a46e206f2b2335c879802a666773d985
abhirup <dev@anantdv.com>
2024-04-30 14:41:06 +0530

    add app

--- trackpro/api.py
+++ trackpro/api.py
@@ -2,10 +2,13 @@ from __future__ import unicode_literals
 import frappe
 import requests
 import json
+from requests.structures import CaseInsensitiveDict
 
 @frappe.whitelist()
 def add_source_lead():
     vid = "25594130"
+    # vid = vehid
+    # vid = "25594130"
     vdn = "BDP-400"
     vdt = "Vehicle"
     
@@ -14,8 +17,8 @@ def add_source_lead():
     payload = {}
     headers = {}
     response = requests.request("GET", url, headers=headers, data=payload)
-    
-    # print(response.text)
+
+
     
     sessiondata = json.loads(response.text)
     # print(sessiondata["eid"])
@@ -31,5 +34,29 @@ def add_source_lead():
     }
     
     response = requests.request("GET", url, headers=headers, data=payload)
-    
-    print(response.text)
+    data = json.loads(response.text)
+    odo = data["item"]["lmsg"]["p"]["odometer"]
+    long = data["item"]["lmsg"]["pos"]["x"]
+    lat = data["item"]["lmsg"]["pos"]["y"]
+    url = f"https://api.geoapify.com/v1/geocode/reverse?lat={lat}&lon={long}&format=json&apiKey=f7a05776fa714b52a1ebff90a3bf5076"
+    payload = {}
+    headers = {}
+    resp = requests.request("GET", url, headers=headers, data=payload)
+    resl = json.loads(resp.text)
+    # print(data.text)
+    loc = resl["results"][0]["formatted"]
+    return odo,lat,long,loc
+    # # return(data)
+    # # print(response.text)
+    # # print(str(data["item"]["lmsg"]["p"]["odometer"]))
+    # url = f"https://api.geoapify.com/v1/geocode/reverse?lat={lat}&lon={long}&apiKey=349cb481664841199c928367b9ef88b9"
+    # # payload = {}
+    # # headers = {}
+
+    # # response = requests.request("GET", url, headers=headers, data=payload)
+    # # print(response.text)
+    # headers = CaseInsensitiveDict()
+    # headers["Accept"] = "application/json"
+    # resp = requests.get(url, headers=headers)
+    # print(resp.text)
+    # return(odo,lat,long)
